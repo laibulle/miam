5c15c8905a82b2f106a7ff29634c08da2f30ff59
Guillaume BAILLEUL <guillaume.bailleul@gmail.com>
2026-09-03 18:28:19 +0200

    draft collaboration

--- app/agent.py
+++ app/agent.py
@@ -5,6 +5,35 @@ from app.tools.get_foods_from_season_tool import get_foods_from_season_tool
 
 MODEL_NAME = "gemini-3.5-flash-lite"
 
+chief_agent = Agent(
+    name="chief_agent",
+    mode="single_turn",
+    instruction="""
+    You are a cooking chief that worked in 3 stars restaurants in the paste. Know your new goal is to help people 
+    cooking tasty and healty food daily. You take benefit of your experience to provide easy and fast tips or 
+    new food combinaison to help people enjoying there self cooked daily meals.
+    """
+)
+
+gut_health_agent = Agent(
+    name="gut_health_agent",
+    mode="single_turn",
+    instruction="""
+    You are a gut health specialist. You need to ensure the recipe will improve microbiome health. 
+    You can make suggesion to improve recipe health rate.  
+    """
+)
+
+nutrition_agent = Agent(
+    name="nutrition_agent",
+    mode="single_turn",
+    instruction="""
+    You are a nutrition specialist. Your goal is to provide a healthy food to eat. You will select ingredients to cook
+    according to the season. You can compute food facts of recipes provided by the chief.
+    """,
+    tools=[get_foods_from_season_tool],
+)
+
 root_agent = Agent(
     model=MODEL_NAME,
     name="root_agent",
@@ -12,10 +41,10 @@ root_agent = Agent(
     output_schema=RecipeResponse,
     input_schema=PromptInput,
     instruction="""
-    Your are a nutrition specialist, you are an excellent cooker and know any recipe of any kind of food.
-    You can compute food facts of your recipes.
-    Provide a recipe to the user according to it requirements and the foods available for the season.
+    You are a meal planner orchestrator, your goal is to coordinates agents to prepare a meal according to the user 
+    input. Ask the nutrition_agent to get foods, the gut_health_agent to make recomandations then chief will prepare
+    a recipe. Ensure all agent have a consensus about the meal.
     Return an error if the user input doesn't ask for a recipe.
     """,
-    tools=[get_foods_from_season_tool],
+    sub_agents=[chief_agent, gut_health_agent, nutrition_agent]
 )
